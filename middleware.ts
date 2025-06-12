ea9f007c1726f6d16cdac5bcc85e0a9d718a4d84
James Birchall <j.pc.birchall@googlemail.com>
2025-06-11 19:16:58 -0500

    Force Vercel to detect and deploy updated middleware

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export function middleware(request: Request) {
   const ua = request.headers.get("user-agent") || "";
   const url = new URL(request.url);
-  console.log(`[EDGE] UA: ${ua} | PATH: ${url.pathname} | METHOD: ${request.method}`);
+  console.log(`[EDGE-NEW] UA: ${ua} | PATH: ${url.pathname} | METHOD: ${request.method}`);
   return NextResponse.next();
 }
 
