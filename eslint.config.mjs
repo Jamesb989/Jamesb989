59dc511d877b704778b04efff40f40a73a10ebc8
James Birchall <j.pc.birchall@googlemail.com>
2025-06-15 22:09:14 -0500

    Update ESLint ignore path

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,6 +10,9 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
+  {
+    ignores: ["pages/api/log-bot.ts"],
+  },
   ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
 
